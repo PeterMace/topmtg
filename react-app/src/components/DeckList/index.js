5b4ad4dc115ca7c4a6883db95e96da1003ca792d
Peter Mace <peterjmace@gmail.com>
2021-08-30 07:19:19 -0700

    deck list component added

--- react-app/src/components/DeckList/index.js
+++ react-app/src/components/DeckList/index.js
@@ -1,64 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
+import { NavLink } from 'react-router-dom';
 import { getDecks } from '../../store/deck';
 
 export const DeckList = () => {
-    const decks = useSelector(state => state.decks);
+    //const decks = useSelector(state => state.decks);
     const dispatch = useDispatch();
-  
-    const onSubmit = async (e) => {
-        e.preventDefault();
-        const data = await dispatch(getDecks());
-        // if (data) {
-        //   setErrors(data)
-        // }
-        console.log(data);
-    };
+    const [decksList, setDeckList] = useState([]);
 
-    useEffect(async () => {
-        const data = await dispatch(getDecks());
-        // if (data) {
-        //   setErrors(data)
-        // }
-        console.log(data);
-        // return () => {
-        //     cleanup
-        // }
+    useEffect(() => {
+        async function fetchData(){
+            const data = await dispatch(getDecks());
+            setDeckList(data.decks);
+        }
+        fetchData();
     }, [])
-
-    
-
-  
-
 
     return (
     <div className="form--container">
-        {/* <form onSubmit ={onSubmit} className="form">
-            <div>
-                {errors.map((error, ind) => (
-                <div key={ind}>{error}</div>
-                ))}
-            </div>
-            <div>
-                <label>Deck Name</label>
-                <input
-                type='text'
-                name='name'
-                onChange={updateName}
-                value={name}
-                ></input>
-            </div>
-            <div>
-                <label>Description</label>
-                <input
-                type='text'
-                name='description'
-                onChange={updateDescription}
-                value={description}
-                ></input>
-            </div>
-            <button type='submit'>Create Deck</button>
-        </form> */}
+        <ul>
+           {decksList.map((deck)=>{
+               return (
+                <NavLink key={deck.id} to={`/decks/${deck.id}`}> {deck.name}
+                <br />
+                {/* <div
+                  className="browser-image"
+                  style={{ backgroundImage: `url('${deck.imageUrl}')` }}
+                ></div> */}
+                </NavLink>
+               )
+           })}
+        </ul>
     </div>
     )
 }
